7980f39d2eb6fe2f77c319c2cb8417b769a1239d
Bill Gilleran <BGilleran@mac.com>
2023-02-06 15:08:54 -0500

    finished app

--- react/pics/src/components/ImageShow.js
+++ react/pics/src/components/ImageShow.js
@@ -1,5 +1,7 @@
-function ImageShow() {
-    return <div>ImageShow</div>;
+import React from 'react';
+
+function ImageShow({key, image}) {
+    return <div><img alt={image.alt_description} src={image.urls.small}/></div>;
 }
 
 export default ImageShow;
